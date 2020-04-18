bef0ec3240af7ac39aeaa3b1536c77749435e95e
高木駿 <tkgsn@takagishunnoMacBook-puro.local>
2020-04-19 07:52:15 +0900

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import sys
+import CallbackServer
+
+def callback_method(query):
+    return ['Hello', 'World!', 'with', query]
+
+if __name__ == '__main__':
+    port = sys.argv[1]
+    CallbackServer.start(port, callback_method)
