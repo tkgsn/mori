fd40af7fd47106a64780e488b7bdd5fe441ab5a5
高木駿 <tkgsn@takagishunnoMacBook-puro.local>
2020-04-21 01:10:25 +0900

    app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def bbs():
+    message = "Hello"
+    return render_template("index.html", message = message)
+
+if __name__ == "__main__":
+    app.run(debug=True)
