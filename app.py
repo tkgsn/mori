97d1058628d2bd923094008d477d293322c4d2ee
高木駿 <tkgsn@takagishunnoMacBook-puro.local>
2020-04-26 14:32:16 +0900

    deploy

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def templates():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='127.0.0.1', port=9876)
